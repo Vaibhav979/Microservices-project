bc2200da66197c67b4d1194ed6b02e560f003f27
vaibhav <vaibhavsingh8931@gmail.com>
2025-10-09 18:30:26 +0530

    added eureka client in HotelService

--- HotelService/HotelService/src/main/java/com/HotelService/Hotel/exception/GlobalExceptionHandler.java
+++ HotelService/HotelService/src/main/java/com/HotelService/Hotel/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.HotelService.Hotel.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
-    
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> notFoundHandler(ResourceNotFoundException ex) {
         Map map = new HashMap();
