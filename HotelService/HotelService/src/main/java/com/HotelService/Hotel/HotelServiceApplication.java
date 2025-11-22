c870ad258ff5b66e3b7ee9f2d3d36e6ead15f2ef
vaibhav <vaibhavsingh8931@gmail.com>
2025-11-22 21:19:44 +0530

    deploy

--- HotelService/HotelService/src/main/java/com/HotelService/Hotel/HotelServiceApplication.java
+++ HotelService/HotelService/src/main/java/com/HotelService/Hotel/HotelServiceApplication.java
@@ -9,5 +9,5 @@ public class HotelServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
 	}
-	// deploy 0.1
+	// deploy 0.2
 }
